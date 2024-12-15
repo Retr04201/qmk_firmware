0f0374a20a85256f704e03fe294d6d9d7052b003
Rishabh Patel <167244608+Retr04201@users.noreply.github.com>
2024-12-15 15:23:32 -0800

    rules.mk
    added slider functions so that you can customize their functions using MIDI mixer

--- keyboards/keebfront/vanguard65/rules.mk
+++ keyboards/keebfront/vanguard65/rules.mk
@@ -1,2 +1,14 @@
 # Processor frequency
 F_CPU = 8000000
+# Rules.mk for Vanguard65 keyboard
+
+# Enable basic features
+MIDI_ENABLE = yes          # Enable MIDI functionality
+RGBLIGHT_ENABLE = yes      # Enable RGB lighting
+NKRO_ENABLE = yes          # Enable N-Key Rollover
+BOOTMAGIC_ENABLE = yes     # Enable Bootmagic key combinations
+TAP_DANCE_ENABLE = yes     # Enable tap dance functionality
+
+# Optional features
+SLEEP_LED_ENABLE = no      # Disable sleep LED functionality
+BACKLIGHT_ENABLE = no      # Disable backlight functionality (use RGBLIGHT instead)
